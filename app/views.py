0e63844677861e95f2e604d177245c9d38d4fef7
alexey <atiunchyk.87@gmail.com>
2021-08-05 17:24:45 +0300

    05/08/2021

--- app/views.py
+++ app/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 # View - функции которые вызываются при переходе на некоторый адрес
+
+def index(request):
+    return render(request,
+        'index.html',
+        {}
+    )
+
