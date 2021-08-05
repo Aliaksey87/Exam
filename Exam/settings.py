846b891cec5ff66cd7f491dff3bb6fb45e132bdf
alexey <atiunchyk.87@gmail.com>
2021-08-05 21:24:33 +0300

    05.08.2021

--- Exam/settings.py
+++ Exam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9qw3dw2n5zk1$4^wa2qcz(w37t^j-2&z44_4&7%9n4y#69tkkp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
