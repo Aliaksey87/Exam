ed605d2a876de7c1e25918e13182176b78205682
alexey <atiunchyk.87@gmail.com>
2021-08-09 23:29:51 +0300

    09.08.2021

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-
 # Create your views here.
 # View - функции которые вызываются при переходе на некоторый адрес
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
 
 def index(request):
     return render(request,
@@ -9,3 +10,26 @@ def index(request):
         {}
     )
 
+def o_chasah(request):
+    return render(request,
+        'o_chasah.html',
+        {}
+    )
+
+def sobiraem_sami(request):
+    return render(request,
+        'sobiraem_sami.html',
+        {}
+    )
+
+def kupit_gotovie(request):
+    return render(request,
+        'kupit_gotovie.html',
+        {}
+    )
+
+def kontakti(request):
+    return render(request,
+        'kontakti.html',
+        {}
+    )
